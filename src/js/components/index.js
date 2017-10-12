b1ff6b9d9f6c3a3994c5e35a787f9efaef2f88e7
yuji nakagawa <nakagawa@nulab.co.jp>
2017-10-12 17:41:00 +0900

    react init

---
+++ src/js/components/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => (
+    <h1>
+        It works!
+    </h1>
+)
+
+render(
+    <App />,
+    document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './js/components/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './js/[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
+          },
+          // {
+          //   loader: 'eslint-loader'
+          // }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3333
+  }
+}
+
+module.exports = config
